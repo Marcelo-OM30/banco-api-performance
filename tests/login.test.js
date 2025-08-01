b15c4c91266af91bdf8c9022edbc57445fa25b7b
Marcelo-OM30 <marcelo.salmeron@om30.com.br>
2025-08-01 16:11:27 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,34 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    iterations: 60,
+    thresholds: {
+        http_req_duration: ['p(90)<10', 'max<1'],
+        http_req_failed: ['rate<0.01'],
+    }
+};
+
+// The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
+export default function () {
+    const url = 'http://localhost:3000/login';
+    const payload = JSON.stringify({
+        username: 'julio.lima',
+        senha: '123456',
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+
+    check(res, {
+        'Validar que o status é 200': (r) => r.status === 200,
+        'Validar que o token é uma string': (r) => typeof r.json().token === 'string',
+    });
+
+    sleep(1);
+}
